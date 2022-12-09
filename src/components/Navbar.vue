fd304ac59e69bca773b507aa633faa3691e15873
Gunbam27 <95085649+Gunbam27@users.noreply.github.com>
2022-12-09 12:30:08 +0900

    -store에서 axios work

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,29 @@
 <template>
     <div className="navbar row-center">
-      <h1>TVUE</h1><input type="text"/><button>SEARCH</button>
+      <h1>TVUE</h1><input @input="keyword=$event.target.value" type="text" id="search" />
+      <button @click="$store.dispatch('getData')">SEARCH</button>
     </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Navbar',
+  data(){
+    return{
+      keyword:'',
+    }
+  },
+  computed: {
+    updateKeyword(){
+      console.log(this.keyword);
+    }
+  },
+  methods:{
+    updateMessage(e){
+      this.$store.commit('keyword',e.target.value);
+      console.log(e.target.value);
+    }
+  },
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -1,9 +1,30 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 const store = createStore({
   state(){
     return {
-      name:'kim',
+        API_KEYS:'c97ea8e3',
+        more:{},
+        SEARCH_VALUE:'frozen',
+        movies:[],
+    }
+  },
+  mutations:{
+    setMore(state,data){
+        state.movies=data;
+    },
+    updateMessage (state, message) {
+        state.movies = message
+      }
+  },
+  actions:{
+    getData(){
+        axios.get(`http://www.omdbapi.com/?apikey=${this.state.API_KEYS}&s=${this.state.SEARCH_VALUE}`)
+        .then((result)=>{
+            console.log(result.data);
+            this.commit('setMore',result.data);
+        })
     }
   },
 })
